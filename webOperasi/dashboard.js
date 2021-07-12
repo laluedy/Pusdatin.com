ed2331b73d638bab02fd1edeed1f02a7f00720e6
laluedy <edylalu33@gmail.com>
2021-07-12 17:41:57 +0800

    menyempurnakan tampilan mobile
    untuk webOperasi dan penyidikan

--- webOperasi/dashboard.js
+++ webOperasi/dashboard.js
@@ -1,41 +1,22 @@
-/* globals Chart:false, feather:false */
-
-(function () {
-  "use strict";
-
-  feather.replace({ "aria-hidden": "true" });
-
-  // Graphs
-  var ctx = document.getElementById("myChart");
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: ["Bandung", "Bantul", "Jombang", "Lombok Tengah", "Ngawi", "Serang", "Situbondo", "Sukabumi"],
-      datasets: [
-        {
-          data: [21797.3, 434.6, 37348, 3593.55, 10867.2, 5062.2, 1320, 4095.79],
-          lineTension: 0,
-          backgroundColor: "transparent",
-          borderColor: "#007bff",
-          borderWidth: 4,
-          pointBackgroundColor: "#007bff",
-        },
-      ],
+Highcharts.chart("container", {
+  data: {
+    table: "datatable",
+  },
+  chart: {
+    type: "column",
+  },
+  title: {
+    text: "Grafik Capaian Operasi Pengamanan Kawasan",
+  },
+  yAxis: {
+    allowDecimals: false,
+    title: {
+      text: "Kegiatan",
     },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: false,
-            },
-          },
-        ],
-      },
-      legend: {
-        display: false,
-      },
+  },
+  tooltip: {
+    formatter: function () {
+      return "<b>" + this.series.name + "</b><br/>" + this.point.y + "#" + this.point.name.toLowerCase();
     },
-  });
-})();
+  },
+});
